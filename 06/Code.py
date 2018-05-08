c62b1f24ac895b9a18f1d72fcb7e5ff691908e02
Jakob Wells <jakobtwells@yahoo.com>
2018-05-07 21:19:18 -0500

    Added the framework for the "Symbol-less" Assembler

---
+++ 06/Code.py
@@ -0,0 +1,36 @@
+# 	An Asm2Hack Assembler - Jakob Wells - May, 2018
+#	Using the laid out class structure provided by the text
+#	Written for Python 2.7.12 (the version that came with Ubuntu)
+#
+#	Code.py - Handles translating mnemonics to bitstrings
+#	Less a class, and more a collection of useful functions and information
+
+class Code:
+
+	destSet = None;
+	compSet = None;
+	jumpSet = None;
+
+	# Returns a 3-bit code corresponding to the input
+	def dest(inDest):
+		outDest = "";
+
+		pass; # Insert Code
+
+		return outDest;
+
+	# Returns a 7-bit code corresponding to the input
+	def comp(inComp):
+		outComp = "";
+
+		pass; # Insert Code
+
+		return outComp;
+
+	# Returns a 3-bit code corresponding to the input
+	def jump(inJump):
+		outJump = "";
+
+		pass; # Insert Code
+
+		return outJump;
---
+++ 06/Parser.py
@@ -0,0 +1,47 @@
+# 	An Asm2Hack Assembler - Jakob Wells - May, 2018
+#	Using the laid out class structure provided by the text
+#	Written for Python 2.7.12 (the version that came with Ubuntu)
+#
+#	Parser.py - Handles input parsing.
+
+class Parser:
+	def __init__(self,fileInput):
+		self.file = fileInput;
+	def hasMoreCommands(self):
+		moreCom = false;
+
+		pass; # Insert Code
+
+		return moreCommands;
+	def advance(self):
+		pass;
+	def commandType(self):
+		comType = "";
+
+		pass; # Insert Code
+
+		return comType;
+	def symbol:
+		outSym = "";
+
+		pass; # Insert Code
+
+		return outSym;
+	def dest(:
+		outDest = "";
+
+		pass; # Insert Code
+
+		return outDest;
+	def comp():
+		outComp = "";
+
+		pass; # Insert Code
+
+		return outComp;
+	def jump():
+		outJump = "";
+
+		pass; # Insert Code
+
+		return outJump;
